7bdf605f059c5d6721299ff144c86b712639e697
safwenHafsawy <hafsawysafwen@gmail.com>
2023-11-16 11:49:05 +0100

    added dashboard protection to middlewares

--- middleware.js
+++ middleware.js
@@ -1,14 +1,24 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-const protectedRoutes = ["/login"];
-
 async function middleware(req) {
   const token = await getToken({ req });
-
   const isAuth = !!token;
 
-  if (isAuth && protectedRoutes.includes(req.nextUrl.pathname)) {
+  //check if user is authenticated and trying to access login page again
+  const authRoutes = new RegExp("/login|/signup");
+  if (isAuth && authRoutes.test(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  //check if user is not admin and trying to access dashboard page
+  const adminRoutes = new RegExp("/dashboard");
+  if (
+    isAuth &&
+    adminRoutes.test(req.nextUrl.pathname) &&
+    token?.userRole !== "ADMIN"
+  ) {
+    console.log("not admin");
     return NextResponse.redirect(new URL("/", req.url));
   }
 }
