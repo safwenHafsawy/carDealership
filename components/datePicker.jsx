288c4f486fa79533379438f2ccf3f679406c3985
safwenHafsawy <hafsawysafwen@gmail.com>
2023-11-20 07:25:14 +0100

    added setStartDate and setEndDate

--- components/datePicker.jsx
+++ components/datePicker.jsx
@@ -3,20 +3,58 @@
 import React, { useState } from "react";
 import Calendar from "./calendar/calendar";
 
-const DatePicker = ({ defaultClass }) => {
-  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
-  const [toggleCalendar, setToggleCalendar] = useState(false);
+const locale = "en-GB";
+const options = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+};
 
-  const onSelect = (day, month, year) => {
-    setDate(year + "-" + month + "-" + day);
+const DatePicker = ({ defaultClass }) => {
+  const [startDate, setStartDate] = useState(
+    new Date().toLocaleString(locale, options)
+  );
+  const [endDate, setEndDate] = useState(
+    new Date().toLocaleString(locale, options)
+  );
+  const [toggleCalendar, setToggleCalendar] = useState([false, null]);
+
+  const onSelect = (year, month, day) => {
+    const newDate = new Date(
+      parseInt(year),
+      parseInt(month),
+      day
+    ).toLocaleString(locale, options);
+
+    if (toggleCalendar[1] === "Start") setStartDate(newDate);
+    else if (toggleCalendar[1] === "End") setEndDate(newDate);
+
+    setToggleCalendar([false, null]);
+  };
+
+  const handleToggleCalendar = (toBeChanged) => {
+    setToggleCalendar([true, toBeChanged]);
   };
 
   return (
     <div className="datepicker">
-      <span onClick={() => setToggleCalendar(true)}>Start Date : {date}</span>{" "}
-      {"  "}
-      <span onClick={() => setToggleCalendar(true)}>End Date : {date}</span>
-      {toggleCalendar ? <Calendar selectDate={onSelect} /> : <></>}
+      <div className="date__container">
+        <span>Start Date : </span>
+        <input
+          onClick={() => handleToggleCalendar("Start")}
+          value={startDate}
+          readOnly
+        />
+      </div>
+      <div className="date__container">
+        <span>End Date : </span>
+        <input
+          onClick={() => handleToggleCalendar("End")}
+          value={endDate}
+          readOnly
+        />
+      </div>
+      {toggleCalendar[0] ? <Calendar selectDate={onSelect} /> : <></>}
     </div>
   );
 };
