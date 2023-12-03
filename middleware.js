5d10200358ea8aa8d6c26f7ffc9a55827e4dd87e
safwenHafsawy <hafsawysafwen@gmail.com>
2023-12-03 07:58:25 +0100

    added a log to check production issue

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 async function middleware(req) {
   const token = await getToken({ req });
+  console.log(token);
   const isAuth = !!token;
 
   //check if user is authenticated and trying to access login page again
