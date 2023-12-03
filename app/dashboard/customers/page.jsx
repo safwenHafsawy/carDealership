b6a47a3d0f17b02ad6c903d48fd1ebaffc4348a0
safwenHafsawy <hafsawysafwen@gmail.com>
2023-12-03 09:34:25 +0100

    added responive design to home page

--- app/dashboard/customers/page.jsx
+++ app/dashboard/customers/page.jsx
@@ -8,29 +8,9 @@ const Customers = () => {
     <section className="adminDashboardSection">
       <DashboardSideNavigation />
       <div className="ListOfCustomers">
-        <SectionHeader>List Of Customers</SectionHeader>
-        <table>
-          <thead>
-            <tr>
-              <th>Customer Name</th>
-              <th>Customer Email</th>
-              <th>Customer Phone</th>
-              <th>Joined At</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Data 1</td>
-              <td>Data 2</td>
-              <td>Data 3</td>
-            </tr>
-            <tr>
-              <td>Data 4</td>
-              <td>Data 5</td>
-              <td>Data 6</td>
-            </tr>
-          </tbody>
-        </table>
+        <SectionHeader type="section_header-dark">
+          This Page is still under constraction
+        </SectionHeader>
       </div>
     </section>
   );
