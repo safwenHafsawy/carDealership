a7ff5e844becf70e9248db79e1d069caac33c4ae
safwenHafsawy <hafsawysafwen@gmail.com>
2023-12-03 07:59:27 +0100

    started creating cusotmer page

--- app/dashboard/customers/page.jsx
+++ app/dashboard/customers/page.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
+import DashboardSideNavigation from "@/components/dashboardComponents/dashboardSideNavigation";
+import { SectionHeader } from "@/components/header";
+
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <section className="adminDashboardSection">
+      <DashboardSideNavigation />
+      <div className="ListOfCustomers">
+        <SectionHeader>List Of Customers</SectionHeader>
+        <table>
+          <thead>
+            <tr>
+              <th>Customer Name</th>
+              <th>Customer Email</th>
+              <th>Customer Phone</th>
+              <th>Joined At</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Data 1</td>
+              <td>Data 2</td>
+              <td>Data 3</td>
+            </tr>
+            <tr>
+              <td>Data 4</td>
+              <td>Data 5</td>
+              <td>Data 6</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 };
 
 export default Customers;
--- components/dashboardComponents/dashboardSideNavigation.jsx
+++ components/dashboardComponents/dashboardSideNavigation.jsx
@@ -2,32 +2,21 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import dashboardHome from "@/public/dashboardIcons/dashboardIcon.svg";
+import carsIcon from "@/public/dashboardIcons/carIcon.svg";
+import customerIcon from "@/public/dashboardIcons/customerIcon.svg";
+
 const DashboardSideNavigation = () => {
   return (
     <div className="dashboard_side_nav">
       <Link href="/dashboard">
-        <Image
-          src="dashboardIcons/dashboardIcon.svg"
-          width={20}
-          height={20}
-          alt="car icon"
-        />
+        <Image src={dashboardHome} width={20} height={20} alt="home icon" />
       </Link>
       <Link href="/dashboard/cars">
-        <Image
-          src="dashboardIcons/carIcon.svg"
-          width={20}
-          height={20}
-          alt="car icon"
-        />
+        <Image src={carsIcon} width={20} height={20} alt="car icon" />
       </Link>
       <Link href="/dashboard/customers">
-        <Image
-          src="dashboardIcons/customerIcon.svg"
-          width={20}
-          height={20}
-          alt="car icon"
-        />
+        <Image src={customerIcon} width={20} height={20} alt="customer icon" />
       </Link>
     </div>
   );
